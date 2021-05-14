a37fb93d2c9f1b3515f7c38cb5ddb601b2f49163
Eugene <clackd1amond@users.noreply.github.com>
2021-05-14 13:10:49 +0300

    index cleanup

--- src/index.js
+++ src/index.js
@@ -8,27 +8,9 @@ import Counter from './counter';
 const store = createStore(reducer);
 const { dispatch } = store;
 
-// const bindActionCreator =
-// 	(creator, dispatch) =>
-// 	(...args) => {
-// 		dispatch(creator(...args));
-// 	};
-
 const { inc, dec, rst, rnd } = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-// const rstDispatch = bindActionCreators(rst, dispatch);
-
-// document.getElementById('inc').addEventListener('click', inc);
-// document.getElementById('dec').addEventListener('click', dec);
-// document.getElementById('rst').addEventListener('click', rst);
-// document.getElementById('rnd').addEventListener('click', () => {
-// 	const value = Math.floor(Math.random() * 10);
-// 	rnd(value);
-// });
 
 const update = () => {
-	// document.getElementById('counter').textContent = store.getState();
 	ReactDOM.render(
 		<Counter
 			counter={store.getState()}
@@ -46,11 +28,3 @@ const update = () => {
 update();
 
 store.subscribe(update);
-
-// store.subscribe(() => {
-// 	console.log(store.getState());
-// });
-
-// store.dispatch({ type: 'INC' });
-// store.dispatch({ type: 'INC' });
-// store.dispatch({ type: 'INC' });

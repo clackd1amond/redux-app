19ec65f0bb495b57888897a608de552d2fb87636
Eugene <clackd1amond@users.noreply.github.com>
2021-05-14 11:21:07 +0300

    dec & rnd actions

--- src/index.js
+++ src/index.js
@@ -3,24 +3,43 @@ const reducer = (state = 0, action) => {
 	switch (action.type) {
 		case 'INC':
 			return state + 1;
+		case 'DEC':
+			return state - 1;
+		case 'RND':
+			return state + action.value;
 		default:
 			return state;
 	}
 };
 
+const inc = () => ({ type: 'INC' });
+const dec = () => ({ type: 'DEC' });
+const rnd = (value) => ({ type: 'RND', value });
+
 const store = createStore(reducer);
 
-store.subscribe(() => {
-	console.log(store.getState());
+document.getElementById('inc').addEventListener('click', () => {
+	store.dispatch(inc());
+});
+document.getElementById('dec').addEventListener('click', () => {
+	store.dispatch(dec());
+});
+document.getElementById('rnd').addEventListener('click', () => {
+	const value = Math.floor(Math.random() * 10);
+	console.log(value);
+	store.dispatch(rnd(value));
 });
 
-store.dispatch({ type: 'INC' });
-store.dispatch({ type: 'INC' });
-store.dispatch({ type: 'INC' });
+const update = () => {
+	document.getElementById('counter').textContent = store.getState();
+};
 
-// let state = reducer(undefined, {});
+store.subscribe(update);
 
-// state = reducer(state, { type: 'INC' });
-// console.log(state);
-// state = reducer(state, { type: 'INC' });
-// console.log(state);
+// store.subscribe(() => {
+// 	console.log(store.getState());
+// });
+
+// store.dispatch({ type: 'INC' });
+// store.dispatch({ type: 'INC' });
+// store.dispatch({ type: 'INC' });
